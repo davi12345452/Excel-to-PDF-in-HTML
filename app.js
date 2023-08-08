ac031c23685edbbab2cfb5845d2bddba6fa56329
DaviMaia <davijanischmaia@gmail.com>
2023-08-07 21:12:19 -0300

    criando em ejs a table

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let Reader = require("./Reader")
 let Processor = require("./Processor")
 let Table = require("./Table")
+let HtmlParser = require("./HtmlParser")
 
 let leitor = new Reader()
 
@@ -9,6 +10,8 @@ const teste = async(file) => {
     let dados= await leitor.Read(file)
     dadosProcess = Processor.Process(dados)
     dadosTabela = new Table(dadosProcess)
+    html = await HtmlParser.Parse(dadosTabela)
+    console.log(html)
 }
 
 teste('./users.csv')
